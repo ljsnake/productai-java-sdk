6d1e74e0e50bfa5595c152bc8ab9818b81ad94b4
Shawn Tian <hightian@gmail.com>
2018-05-28 14:12:36 +0800

    refactor junit test for JUnit5

---
+++ src/test/java/cn/productai/apiv2/ProductSearchTest.java
@@ -0,0 +1,13 @@
+package cn.productai.apiv2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductSearchTest {
+
+    @Test
+    void test() {
+        assertEquals(2, 1 + 1);
+    }
+}
