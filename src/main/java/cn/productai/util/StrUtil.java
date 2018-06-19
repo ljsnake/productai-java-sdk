560a1aa2a01848e4e9cf36385d49c2568d17f5cb
Shawn Tian <hightian@gmail.com>
2018-06-19 23:24:47 +0800

    fix bug of loop list

--- src/main/java/cn/productai/util/StrUtil.java
+++ src/main/java/cn/productai/util/StrUtil.java
@@ -9,7 +9,7 @@ public class StrUtil {
         if (list == null) return "";
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             if (i > 0) {
                 sb.append(delimiter);
             }
